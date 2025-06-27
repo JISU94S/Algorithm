f65cefdac6fb3d803197d85037c51d5c63971b97
JISU <tjswltn0901@gmail.com>
2025-06-27 22:40:55 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.01 ms
+메모리: 4.11 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 22:32:03
+2025년 06월 27일 22:40:50
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -22,7 +22,11 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     }
     
      for(int i = 0; i < lost.size(); i++){
+        if(lost[i] == -1) continue;
+         
         for(int j = 0; j < reserve.size(); j++){
+            if(reserve[j] == -1) continue;
+            
             if(abs(lost[i] - reserve[j]) == 1){
                 lost[i] = -1;
                 reserve[j] = -1;
